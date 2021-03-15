7d05e9b6f5843fde9f8d7e018b2daea3cd50b2bf
算神 <lishewen@msn.com>
2021-03-15 08:47:00 +0800

    注释

--- Blazor.ECharts/Options/Series/Candlestick/Candlestick.cs
+++ Blazor.ECharts/Options/Series/Candlestick/Candlestick.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Blazor.ECharts.Options.Series.Candlestick
 {
+    /// <summary>
+    /// K线图
+    /// </summary>
     public class Candlestick : SeriesBase
     {
         public Candlestick() : base("candlestick") { }
