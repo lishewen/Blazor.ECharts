3b08bc00260fe972aedb86659b0aa77c73063420
黎摄文 <lishewen@gmail.com>
2021-03-20 08:38:32 +0800

    获取随机颜色

--- Blazor.ECharts.Demo/Utils/Util.cs
+++ Blazor.ECharts.Demo/Utils/Util.cs
@@ -7,30 +7,14 @@ namespace Blazor.ECharts.Demo.Utils
 {
     public static class Util
     {
+        /// <summary>
+        /// 获取随机颜色
+        /// </summary>
+        /// <returns></returns>
         public static string GetRandomColor()
         {
-            Random tempR = new();
-            Random rR = new((int)DateTime.Now.Ticks);
-            System.Threading.Thread.Sleep(tempR.Next(50));
-            Random rG = new((int)DateTime.Now.Ticks);
-            System.Threading.Thread.Sleep(tempR.Next(50));
-            Random rB = new((int)DateTime.Now.Ticks);
-            string colorR = Convert.ToString(rR.Next(rB.Next()) % 16, 16);
-            string colorG = Convert.ToString(rG.Next(rR.Next()) % 16, 16);
-            string colorB = Convert.ToString(rB.Next(rR.Next()) % 16, 16);
-            if (colorR == "0")
-            {
-                colorR = "00";
-            }
-            if (colorG == "0")
-            {
-                colorG = "00";
-            }
-            if (colorB == "0")
-            {
-                colorB = "00";
-            }
-            return "#" + colorR + colorG + colorB;
+            Random random = new();
+            return string.Format("#{0:X6}", random.Next(0x1000000));
         }
     }
 }
