ae98f279b71e0c8ed5268432390e44d5bbbed56e
黎摄文 <me@lishewen.com>
2021-08-12 06:41:32 +0800

    JFuncConverter

---
+++ Blazor.ECharts/JFuncConverter.cs
@@ -0,0 +1,24 @@
+﻿using Blazor.ECharts.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Blazor.ECharts
+{
+    public class JFuncConverter : JsonConverter<JFunc>
+    {
+        public override JFunc Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(Utf8JsonWriter writer, JFunc value, JsonSerializerOptions options)
+        {
+            writer.WriteRawValue(value.RAW, true);
+        }
+    }
+}
