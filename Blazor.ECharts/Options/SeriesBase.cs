6da885b1119dece33c6defb63f442f1dcfebf78b
算神 <lishewen@msn.com>
2022-09-21 15:44:08 +0800

    series添加zlevel属性 #13

--- Blazor.ECharts/Options/SeriesBase.cs
+++ Blazor.ECharts/Options/SeriesBase.cs
@@ -41,5 +41,12 @@ namespace Blazor.ECharts.Options
         /// <para>点击<see href="https://www.echartsjs.com/zh/option.html#series-line.data ">此处</see>查看详细设置</para>
         /// </summary>
         public object Data { set; get; }
+
+        /// <summary>
+        /// 所有图形的 zlevel 值。
+        /// <para>zlevel用于 Canvas 分层，不同zlevel值的图形会放置在不同的 Canvas 中，Canvas 分层是一种常见的优化手段。我们可以把一些图形变化频繁（例如有动画）的组件设置成一个单独的zlevel。需要注意的是过多的 Canvas 会引起内存开销的增大，在手机端上需要谨慎使用以防崩溃。</para>
+        /// <para>zlevel 大的 Canvas 会放在 zlevel 小的 Canvas 的上面。</para>
+        /// </summary>
+        public int? Zlevel { set; get; }
     }
 }
