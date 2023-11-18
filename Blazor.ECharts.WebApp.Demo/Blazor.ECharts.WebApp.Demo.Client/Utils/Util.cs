235f2d64dbd730f51d9327b6aa7674105d0eadfa
算神 <lishewen@msn.com>
2023-11-18 17:37:43 +0800

    词云例子

---
+++ Blazor.ECharts.WebApp.Demo/Blazor.ECharts.WebApp.Demo.Client/Utils/Util.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.ECharts.WebApp.Demo.Client.Utils
+{
+    public static class Util
+    {
+        /// <summary>
+        /// 获取随机颜色
+        /// </summary>
+        /// <returns></returns>
+        public static string GetRandomColor()
+        {
+            return string.Format("#{0:X6}", Random.Shared.Next(0x1000000));
+        }
+    }
+}
